06db6de7af34661600a31c5d5b8e9342b39e944b
jeff <jeff.etale@student.moringaschool.com>
2023-09-25 20:05:26 +0300

    done lab

--- server/app.py
+++ server/app.py
@@ -17,11 +17,26 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        plants = Plant.query.all()
+        return make_response(jsonify([plant.to_dict() for plant in plants]), 200)
+
+    def post(self):
+        data = request.get_json()
+        new_plant = Plant(name=data['name'], image=data['image'], price=data['price'])
+        db.session.add(new_plant)
+        db.session.commit()
+        return make_response(jsonify(new_plant.to_dict()), 201)
 
 class PlantByID(Resource):
-    pass
-        
+    def get(self, id):
+        plant = Plant.query.get(id)
+        if plant is None:
+            return make_response(jsonify({'error': 'Plant not found'}), 404)
+        return make_response(jsonify(plant.to_dict()), 200)
+
+api.add_resource(Plants, '/plants')
+api.add_resource(PlantByID, '/plants/<int:id>')        
 
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(port=5000, debug=True)
